74805b15610170fda62f093ab5457ae4fa8ae4df
Alex Monroe <alexrmonroe@gmail.com>
2025-08-06 08:41:21 -0700

    chore: add daily playwright run

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from '@playwright/test';
 
+const baseURL =
+  process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000';
+
+const webServer = process.env.PLAYWRIGHT_TEST_BASE_URL
+  ? undefined
+  : {
+      command: 'npm run dev -- --port=3000',
+      port: 3000,
+      reuseExistingServer: !process.env.CI,
+      timeout: 120 * 1000,
+    };
+
 export default defineConfig({
   testDir: './tests',
-  webServer: {
-    command: 'npm run dev -- --port=3000',
-    port: 3000,
-    reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
-  },
+  ...(webServer ? { webServer } : {}),
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL,
     headless: true,
   },
 });
