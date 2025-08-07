d0aa0fbeb1f77bc5bcc38a7105b21da276f1f403
Alex Monroe <alexrmonroe@gmail.com>
2025-08-06 17:04:26 -0700

    Improve mobile responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
-        <nav className="p-4 space-x-4">
+        <nav className="p-4 flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-4">
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
           <a
